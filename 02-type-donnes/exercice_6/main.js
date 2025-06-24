b01ea34a760cd450d1b633382a17fb811b6a3faa
Chris Mwanya <70743689+ChrisMwanya@users.noreply.github.com>
2025-06-24 15:25:43 +0100

    Create main.js

---
+++ 02-type-donnes/exercice_6/main.js
@@ -0,0 +1,14 @@
+let age = 16;Add commentMore actions
+let nom = "KALALA";
+let estConnecte = true ;
+let valeurNulle = null;
+let inconnue ;
+
+// Les lignes ne doivent pas être modifiéesAdd commentMore actions
+module.exports = {
+  age,
+  nom,
+  estConnecte,
+  valeurNulle,
+  inconnue,
+};
