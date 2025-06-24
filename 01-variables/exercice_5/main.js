170f864f9129f74257d668df0386cca94a07f4b0
Chris Mwanya <70743689+ChrisMwanya@users.noreply.github.com>
2025-06-24 15:22:58 +0100

    Create main.js

---
+++ 01-variables/exercice_5/main.js
@@ -0,0 +1,10 @@
+let celsius = 10;
+
+// Conversion Celsius à FahrenheitAdd commentMore actions
+function celsiusToFahrenheit(celsius) {
+  // ecrire le code de conversion ici
+   let fahrenheit = (celsius * 9/5)+32;
+  console.log(fahrenheit)
+  // la ligne suivante ne doit pas être modifiée
+  return fahrenheit;
+}
